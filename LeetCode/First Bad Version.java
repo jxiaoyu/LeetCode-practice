fec207c3c00193ba037d999fc1d48babece33517
river <1990jiangxiaoyu@163.com>
2016-06-11 17:27:32 +0800

    First Bad Version

---
+++ LeetCode/First Bad Version.java
@@ -0,0 +1,24 @@
+/**
+ * Created by jxiao on 2016/6/11.
+ */
+public class Solution extends VersionControl {
+
+    public int firstBadVersion(int n) {
+
+        int left = 1, right = n, mid;
+
+        while (true) {
+
+            // 技巧1：当 left, right 都很大的时候，left + (right - left) / 2 比 (right + left) / 2 快很多
+            mid = left + (right - left) / 2;
+            if (mid == left) {
+                return isBadVersion(left) ? left : right;
+            }
+            if (isBadVersion(mid)) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+    }
+}

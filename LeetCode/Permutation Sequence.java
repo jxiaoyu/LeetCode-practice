71645aa5aafb5d0945546850e098e9d04c6c4b8b
river <1990jiangxiaoyu@163.com>
2016-06-13 01:21:54 +0800

    Permutation Sequence

---
+++ LeetCode/Permutation Sequence.java
@@ -0,0 +1,39 @@
+
+/**
+ * Created by jxiao on 2016/6/11.
+ */
+public class Solution {
+
+    public String getPermutation(int n, int k) {
+
+        int fac = 1;
+        for (int i = 1; i < n; i++) {
+            fac *= i;
+        }
+
+        StringBuilder builder = new StringBuilder();
+        boolean[] used = new boolean[n];
+        // 注意1：计数的问题
+        k = k - 1;
+
+        for (int i = 0; i < n; i++) {
+            int d = k / fac;
+            k = k % fac;
+            for (int j = 0; j < n; j++) {
+                if (!used[j] && d == 0) {
+                    builder.append(j + 1);
+                    used[j] = true;
+                    break;
+                } else if (!used[j]) {
+                    d--;
+                }
+            }
+
+            if (i < n - 1) {
+                fac /= (n - i - 1);
+            }
+        }
+
+        return builder.toString();
+    }
+}

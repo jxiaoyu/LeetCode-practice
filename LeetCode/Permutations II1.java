7a71ab5a0733961bac43318e4ad7cb283019beaa
river <1990jiangxiaoyu@163.com>
2016-06-12 17:33:28 +0800

    Permutations II

---
+++ LeetCode/Permutations II1.java
@@ -0,0 +1,31 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Created by river on 2016/6/12.
+ */
+public class Solution {
+    public List<List<Integer>> permuteUnique(int[] nums) {
+        List<List<Integer>> perms = new ArrayList<List<Integer>>();
+        perms.add(new ArrayList<Integer>());
+        for (int i = 0; i < nums.length; i++) {
+            List<List<Integer>> tmpPerms = new ArrayList<List<Integer>>(perms);
+            Map<List<Integer>, Boolean> map = new HashMap<List<Integer>, Boolean>();
+            perms.clear();
+            for (List<Integer> list : tmpPerms) {
+                for (int j = 0; j <= list.size(); j++) {
+                    List<Integer> tmp = new ArrayList<Integer>(list);
+                    tmp.add(j, nums[i]);
+                    Boolean had = map.get(tmp);
+                    if (had == null || !had) {
+                        perms.add(tmp);
+                        map.put(tmp, true);
+                    }
+                }
+            }
+        }
+        return perms;
+    }
+}

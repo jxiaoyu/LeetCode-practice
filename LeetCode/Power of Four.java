abd0f98b949853699ef3c0bd1896b9932b81167e
river <1990jiangxiaoyu@163.com>
2016-06-13 13:59:45 +0800

    增加注释

--- LeetCode/Power of Four.java
+++ LeetCode/Power of Four.java
@@ -1,6 +1,12 @@
 /**
  * Created by river on 2016/6/13.
  */
+
+/**
+ * https://segmentfault.com/a/1190000003481153
+ * 提供了非循环的解题思路。主要用到了二进制就是以 2 为底的特点
+ */
+
 public class Solution {
     public boolean isPowerOfFour(int num) {
         // 注意1：对负数的处理。power of 4 是正数

3cb0c879a73c0631b7619028866f881a7c75a414
river <1990jiangxiaoyu@163.com>
2016-06-17 13:52:11 +0800

    Remove Linked List Elements

---
+++ LeetCode/Remove Linked List Elements.java
@@ -0,0 +1,21 @@
+/**
+ * 考察链表的操作
+ */
+public class Solution {
+    public ListNode removeElements(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode pre = dummy;
+
+        while (head != null) {
+            if (head.val == val) {
+                pre.next = head.next;
+            } else {
+                pre  = head;
+            }
+            head = head.next;
+        }
+
+        return dummy.next;
+    }
+}

e1eae1c3717607a5680d9fb9767b9714d6010ef4
river <1990jiangxiaoyu@163.com>
2016-06-17 17:57:58 +0800

    修改

--- LeetCode/Binary Tree Inorder Traversal.java
+++ LeetCode/Binary Tree Inorder Traversal.java
@@ -13,10 +13,10 @@ import java.util.Stack;
  */
 public class Solution {
     public List<Integer> inorderTraversal(TreeNode root) {
-        if (root == null) {
-            return null;
-        }
         List<Integer> list = new ArrayList<Integer>();
+        if (root == null) {
+            return list;
+        }
         Stack<TreeNode> stack = new Stack<TreeNode>();
         TreeNode p = root;
         while (!stack.empty() || p != null) {

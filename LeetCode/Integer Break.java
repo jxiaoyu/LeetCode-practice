f09d19464b79182c800bf42869b97b374ed777aa
river <1990jiangxiaoyu@163.com>
2016-06-17 13:17:52 +0800

    修改

--- LeetCode/Integer Break.java
+++ LeetCode/Integer Break.java
@@ -4,9 +4,6 @@
 
 public class Solution {
     public int integerBreak(int n) {
-        if (n <= 2) {
-            return 1;
-        }
         int[] records = new int[n+1];
         for (int i = 1; i < n + 1; i++) {
             for (int j = 1; j <= i / 2; j++) {

0891c4d9acaa995abd6e051f4bec0aa08607441b
river <1990jiangxiaoyu@163.com>
2016-06-17 17:59:49 +0800

    Binary Tree Preorder Traversal

---
+++ LeetCode/Binary Tree Preorder Traversal.java
@@ -0,0 +1,34 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Solution {
+    public List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> list = new ArrayList<Integer>();
+        if (root == null) {
+            return list;
+        }
+        Stack<TreeNode> stack = new Stack<TreeNode>();
+        stack.push(root);
+        while (!stack.empty()) {
+            TreeNode p = stack.pop();
+            list.add(p.val);
+            if (p.right != null) {
+                stack.push(p.right);
+            }
+            if (p.left != null) {
+                stack.push(p.left);
+            }
+        }
+        return list;
+    }
+}

32bd657d9f280afc2c1326ea9fd0113213286d26
river <1990jiangxiaoyu@163.com>
2016-07-02 00:58:25 +0800

    House Robber

---
+++ LeetCode/House Robber.java
@@ -0,0 +1,21 @@
+/**
+ * 简单的 dp 问题
+ */
+public class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) {
+            return 0;
+        }
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(dp[0], nums[1]);
+        
+        for (int i = 2; i < nums.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i-2], dp[i-1]);
+        }
+        return dp[nums.length-1];
+    }
+}

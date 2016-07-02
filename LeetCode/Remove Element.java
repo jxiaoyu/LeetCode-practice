3e0cf1babe3e55f2f7bb910a0fa3e5e4b57f7172
river <1990jiangxiaoyu@163.com>
2016-07-02 19:35:50 +0800

    Remove Element

---
+++ LeetCode/Remove Element.java
@@ -0,0 +1,14 @@
+public class Solution {
+    public int removeElement(int[] nums, int val) {
+        int i = 0, j = nums.length - 1;
+        while (i <= j) {
+            if (nums[i] != val) {
+                i++;
+                continue;
+            }
+            nums[i] = nums[j];
+            j--;
+        }
+        return j + 1;
+    }
+}

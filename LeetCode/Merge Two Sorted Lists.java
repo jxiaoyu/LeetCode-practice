0aff7abb69abe15bda9bf43df03275e6fcb26e71
river <1990jiangxiaoyu@163.com>
2016-07-10 18:17:09 +0800

    修改

--- LeetCode/Merge Two Sorted Lists.java
+++ LeetCode/Merge Two Sorted Lists.java
@@ -1,38 +1,31 @@
-import java.util.*;
-
 /**
- * Created by jxiao on 2016/6/11.
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
  */
 public class Solution {
     public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        ListNode prev = null;
-        ListNode head = null;
-        while(l1 != null || l2 != null) {
-            ListNode newNode = null;
-            if (l1 == null) {
-                newNode = new ListNode(l2.val);
-                l2 = l2.next;
-            }
-            else if (l2 == null) {
-                newNode = new ListNode(l1.val);
+        ListNode dummy = new ListNode(0);
+        dummy.next = l1;
+        ListNode prev = dummy;
+        while (l1 != null && l2 != null) {
+            if (l1.val < l2 .val) {
+                prev = l1;
                 l1 = l1.next;
-            }
-            else if (l1.val < l2.val) {
-                newNode = new ListNode(l1.val);
-                l1 = l1.next;
-            }
-            else {
-                newNode = new ListNode(l2.val);
+            } else {
+                prev.next = l2;
+                prev = l2;
                 l2 = l2.next;
+                prev.next = l1;
             }
-            if (prev == null) {
-                head = newNode;
-            }
-            else {
-                prev.next = newNode;
-            }
-            prev = newNode;
         }
-        return head;
+
+        if (l1 == null) {
+            prev.next = l2;
+        }
+        return dummy.next;
     }
 }

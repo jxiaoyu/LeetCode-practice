53a4a9a3879bc319f4cac7fa186f012a1f84eb0e
river <1990jiangxiaoyu@163.com>
2016-07-13 01:53:54 +0800

    Pow(x, n)

---
+++ LeetCode/Pow(x, n).java
@@ -0,0 +1,27 @@
+/**
+ * 这题并吧难，思想上注意要用折半的方法，这样时间复杂度 lg(n)
+ * 但是还是有细节值得注意。n 是负数的时候要转成 long 型再转正
+ */
+public class Solution {
+    public double myPow(double x, int n) {
+        if (n < 0) {
+            return myPowHelper(1 / x, -(long)n);
+        } else {
+            return myPowHelper(x, n);
+        }
+    }
+    
+    private double myPowHelper(double x, long n) {
+        if (n == 0) {
+            return 1;
+        } else if (n == 1) {
+            return x;
+        }
+        double result = myPowHelper(x, n / 2);
+        if (n % 2 == 1) {
+            return result * result * x;
+        } else {
+            return result * result;
+        }
+    }
+}

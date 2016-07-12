a680eb2b3df403844053f208a8820d036d04ea1c
river <1990jiangxiaoyu@163.com>
2016-07-13 01:19:15 +0800

    Valid Perfect Square

---
+++ LeetCode/Valid Perfect Square.java
@@ -0,0 +1,12 @@
+/**
+ * 牛顿法求平方根
+ */
+public class Solution {
+    public boolean isPerfectSquare(int num) {
+        long r = num;
+        while (r * r > num) {
+            r = (r + num / r) / 2;
+        }
+        return r * r == num;
+    }
+}

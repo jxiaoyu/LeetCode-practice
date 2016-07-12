7cf23df68328362393b43be09959927bdd7894a8
river <1990jiangxiaoyu@163.com>
2016-07-13 01:35:52 +0800

    Sqrt(x)

---
+++ LeetCode/Sqrt(x).java
@@ -0,0 +1,12 @@
+/**
+ * 牛顿法求平方根
+ */
+public class Solution {
+    public int mySqrt(int x) {
+        long r = x;
+        while (r * r > x) {
+            r = (r + x / r) / 2;
+        }
+        return (int)r;
+    }
+}

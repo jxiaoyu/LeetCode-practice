b46de24475865d0174aa3c1d70453192c2162203
river <1990jiangxiaoyu@163.com>
2016-07-13 16:54:33 +0800

    Find Minimum in Rotated Sorted Array

---
+++ LeetCode/Find Minimum in Rotated Sorted Array.java
@@ -0,0 +1,20 @@
+/**
+ * 刚做完 Find Peak Element，借鉴下思路可以解决
+ */
+public class Solution {
+    public int findMin(int[] nums) {
+        int l = 0, r = nums.length - 1;
+        if (nums[l] < nums[r]) {
+            return nums[l];
+        }
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+            if (nums[mid] > nums[r]) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+        return nums[r];
+    }
+}

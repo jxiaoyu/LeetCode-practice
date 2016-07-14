d45aed7cab01334216e3f43242981a03f927be8b
river <1990jiangxiaoyu@163.com>
2016-07-15 01:11:06 +0800

    Longest Increasing Subsequence

---
+++ LeetCode/Longest Increasing Subsequence1.java
@@ -0,0 +1,27 @@
+/**
+ * 时间复杂度 O(nlgn)
+ */
+public class Solution {
+    public int lengthOfLIS(int[] nums) {
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (i == 0) {
+                list.add(nums[i]);
+            } else if (nums[i] > list.get(list.size() - 1)) {
+                list.add(nums[i]);
+            } else {
+                int l = 0, r = list.size() - 1;
+                while (l < r) {
+                    int mid = l + (r - l) / 2;
+                    if (nums[i] > list.get(mid)) {
+                        l = mid + 1;
+                    } else {
+                        r = mid;
+                    }
+                }
+                list.set(l, nums[i]);
+            }
+        }
+        return list.size();
+    }
+}

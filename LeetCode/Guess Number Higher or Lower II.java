4e135664e08f49d833ed82bffbfdc60d52e6da4c
river <1990jiangxiaoyu@163.com>
2016-07-21 01:26:59 +0800

    Guess Number Higher or Lower II

---
+++ LeetCode/Guess Number Higher or Lower II.java
@@ -0,0 +1,27 @@
+/**
+ * 一开始的想法是二分，在二分的基础上做一些修正
+ * 但是推演了 1 到 6 的几种情况，发现没有修正的依据，只能遍历
+ * 需要 dp 保存结果，避免重复计算
+ */
+public class Solution {
+    
+    public int getMoneyAmount(int n) {
+        int[][] dp = new int[n+1][n+1];
+        return solve(1, n, dp);
+    }
+    
+    private int solve(int l, int r, int[][] dp) {
+        if (l >= r) {
+            return 0;
+        }
+        if (dp[l][r] != 0) {
+            return dp[l][r];
+        }
+        int min = Integer.MAX_VALUE;
+        for (int i = l; i <= r; i++) {
+            min = Math.min(min, Math.max(solve(l, i - 1, dp), solve(i + 1, r, dp)) + i);
+        }
+        dp[l][r] = min;
+        return min;
+    }
+}

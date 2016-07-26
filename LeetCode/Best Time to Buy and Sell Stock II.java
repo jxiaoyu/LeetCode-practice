3e0c74d6b9ea1c45b83b4bedb817c64160415df8
river <1990jiangxiaoyu@163.com>
2016-07-27 00:39:07 +0800

    修改注释

--- LeetCode/Best Time to Buy and Sell Stock II.java
+++ LeetCode/Best Time to Buy and Sell Stock II.java
@@ -1,6 +1,5 @@
 /** 
  * 一开始看到觉得比 Best Time to Buy and Sell Stock 更容易啊，稍微改一下就提交了，超时了
- * 后来才发现这题一天是允许卖了又买的，求出上升序列的差的和即可。时间复杂度 O(n) 比 dp 小
  */
 public class Solution {
     public int maxProfit(int[] prices) {

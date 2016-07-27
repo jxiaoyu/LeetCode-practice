18a9d646c69a8fb0ce41bfd625da389cb2c0ff40
river <1990jiangxiaoyu@163.com>
2016-07-27 15:17:28 +0800

    Palindrome Partitioning II

---
+++ LeetCode/Palindrome Partitioning II.java
@@ -0,0 +1,32 @@
+/**
+ * 之前已经总结过了对于像 Palindrome Partitioning 那种要求出所有解的题目，遍历是必须的，这里把 I 稍微改一下提交果不其然超时了
+ * 分两步
+ * 1. 用 dp 求出任意一个区间是否是回文，这优化了 I 中对每个字符串都要遍历一遍的问题
+ * 2. 用 dp 求出到当前字符所需的最小切分数，这优化了 I 的 dfs
+ * 
+ * 想不清楚可以分两步写，写完发现可以整合在一起
+ */
+public class Solution {
+    public int minCut(String s) {
+        if (s == null || s.length() == 0) {
+            return 0;
+        }
+        boolean[][] dp = new boolean[s.length()][s.length()];
+        dp[0][0] = true;
+        int[] cut = new int[s.length()];
+        for (int j = 1; j < s.length(); j++) {
+            cut[j] = j;
+            for (int i = j; i >= 0; i--) {
+                if (s.charAt(i) == s.charAt(j) && (i + 1 >= j || dp[i+1][j-1])) {
+                    dp[i][j] = true;
+                    if (i == 0) {
+                        cut[j] = 0;
+                    } else {
+                        cut[j] = Math.min(cut[j], cut[i-1] + 1);
+                    }
+                }
+            }
+        }
+        return cut[s.length()-1];
+    }
+}

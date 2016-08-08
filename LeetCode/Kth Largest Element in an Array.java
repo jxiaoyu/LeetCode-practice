c2e41dcb9e06b7de6207e2cb2d23ceba939ef9c4
river <1990jiangxiaoyu@163.com>
2016-08-08 09:58:46 +0800

    Kth Largest Element in an Array

---
+++ LeetCode/Kth Largest Element in an Array.java
@@ -0,0 +1,15 @@
+/**
+ * 时间复杂度 nlg(k)
+ */
+public class Solution {
+    public int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> q = new PriorityQueue<Integer>(k);
+        for (int i: nums) {
+            q.offer(i);
+            if (q.size() > k) {
+                q.poll();
+            }
+        }
+        return q.peek();
+    }
+}

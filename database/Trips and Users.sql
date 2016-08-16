8b4556295a24c40d4552a938277c06a1a735e029
river <1990jiangxiaoyu@163.com>
2016-08-16 15:15:53 +0800

    Trips and Users

---
+++ database/Trips and Users.sql
@@ -0,0 +1,23 @@
+# Write your MySQL query statement below
+SELECT
+    t.Request_at DAY,
+    round(
+        sum(
+            CASE
+            WHEN t. STATUS LIKE 'cancelled_%' THEN
+                1
+            ELSE
+                0
+            END
+        ) / count(*),
+        2
+    ) Cancellation Rate
+FROM
+    Trips t
+INNER JOIN Users u ON t.Client_Id = u.Users_Id
+AND u.Banned = 'No'
+WHERE
+    t.Request_at BETWEEN '2013-10-01'
+AND '2013-10-03'
+GROUP BY
+    t.Request_at

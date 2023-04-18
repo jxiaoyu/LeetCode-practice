3a068835307dc0e0799c4a001b855df5cbe31b4f
chongyue.jxy <chongyue.jxy@alibaba-inc.com>
2023-04-18 14:44:24 +0800

    Print Zero Even Odd.java

--- conc/Print Zero Even Odd.java
+++ conc/Print Zero Even Odd.java
@@ -1,3 +1,6 @@
+import java.util.concurrent.Semaphore;
+import java.util.function.IntConsumer;
+
 class ZeroEvenOdd {
     private int n;
 
@@ -5,37 +8,37 @@ class ZeroEvenOdd {
         this.n = n;
     }
 
-    private volatile boolean printZero = true;
+    private Semaphore evenSem = new Semaphore(0);
+    private Semaphore oddSem = new Semaphore(0);
+    private Semaphore zeroSem = new Semaphore(1);
 
-    private volatile int index = 1;
 
     // printNumber.accept(x) outputs "x", where x is an integer.
     public void zero(IntConsumer printNumber) throws InterruptedException {
-        while (index < n) {
-            if (printZero) {
-                printNumber.accept(0);
-                printZero = false;
+        for (int i = 1; i <= n; i++) {
+            zeroSem.acquire();
+            printNumber.accept(0);
+            if (i % 2 == 0) {
+                evenSem.release();
+            } else {
+                oddSem.release();
             }
         }
     }
 
     public void even(IntConsumer printNumber) throws InterruptedException {
-        while (index <= n) {
-            if (index % 2 == 0 && !printZero) {
-                printNumber.accept(index);
-                printZero = true;
-                index++;
-            }
+        for (int i = 2; i <= n; i = i + 2) {
+            evenSem.acquire();
+            printNumber.accept(i);
+            zeroSem.release();
         }
     }
 
     public void odd(IntConsumer printNumber) throws InterruptedException {
-        while (index <= n) {
-            if (index % 2 == 1 && !printZero) {
-                printNumber.accept(index);
-                printZero = true;
-                index++;
-            }
+        for (int i = 1; i <= n; i = i + 2) {
+            oddSem.acquire();
+            printNumber.accept(i);
+            zeroSem.release();
         }
     }
 }
